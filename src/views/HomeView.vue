9ecfd9b7ab45b45faa1fffa0361ace6a7f9f7ac3
lamhoangduyen2k2 <lamhoangduyen9a4@gmail.com>
2024-12-07 00:02:51 +0700

    7.32

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <HelloWorld />
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid>
+      <div class="head"></div>
+    </v-container>
+  </v-app>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-
+<script setup>
 // Components
-import HelloWorld from "../components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "HomeView",
-
-  components: {
-    HelloWorld,
-  },
-});
+import NavBar from "@/components/NavBar.vue";
 </script>
+
+<style scoped>
+.v-container {
+  padding: 16px 0;
+}
+.head {
+  position: relative;
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width: 100%;
+  color: white;
+}
+.head:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+}
+</style>
